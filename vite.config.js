fa84f5f95704d4397a1980356c97b34ed3d714c5
Emmanuel Balogun <emmanx25@gmail.com>
2024-04-21 01:24:04 +0100

    Add Vite PWA plugin to package.json and vite.config.js
    update logo.png

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true
+      },
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: "SummAI",
+        short_name: "SummAI",
+        description:
+          "SummAI is an article summarizer that turns long articles, blog posts, and other text documents into condensed readable summaries, so you can read with less effort.",
+        theme_color: "#141A46",
+        background_color: "#141A46",
+        icons: [
+          {
+            src: "/logo.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
