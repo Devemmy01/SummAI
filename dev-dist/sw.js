14cd03c28b3bcc00b0fc3b69267363840409253a
Emmanuel Balogun <emmanx25@gmail.com>
2024-05-26 23:26:25 +0100

    scrollbar adjust

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ele7987gd58"
+    "revision": "0.pesg1ur8g28"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
